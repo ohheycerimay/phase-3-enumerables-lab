57e9848423841414003bd771b0bc232549a164e2
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-11-28 15:34:51 -0500

    commit tree c23920be54f7757a7c96d60a33a498a7b0c0c63d

--- enumerables.rb
+++ enumerables.rb
@@ -12,7 +12,9 @@ end
 # given an array of spicy foods, **return an array of strings**
 # with the names of each spicy food
 def get_names(spicy_foods)
-  # your code here
+  spicy_foods.map do |food|
+    food[:name]
+  end
 end
 
 # given an array of spicy foods, **return an array of hashes** 
